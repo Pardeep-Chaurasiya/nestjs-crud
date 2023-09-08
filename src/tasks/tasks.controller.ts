ca1284c6e3d378328e1d54e17296d3ff9bc85a07
Pardeep Chaurasiya <chaurasiyapardeep001@gmail.com>
2023-09-08 16:08:47 +0530

    nestjs crud

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,36 @@
+import { Controller,Get,Post,Body,Param, Delete, Patch } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private taskService:TasksService){}
+
+    @Get()
+    getAllTasks():Task[]{
+        return this.taskService.getAllTask()
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id') id:string){
+        return this.taskService.getTaskById(id)
+    }
+
+    @Post()
+    createTask(@Body('title') title:string,
+    @Body('description') description:string):Task{
+        return this.taskService.createTask(title,description)
+    }
+
+    @Patch('/:id')
+    updateTask(@Param('id') id:string,
+    @Body('status') status:TaskStatus){
+        return this.taskService.updateTask(id,status)
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id:string){
+        this.taskService.deleteTask(id);
+        return `task ${id} is deleted`
+    }
+}
